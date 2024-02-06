4490eeebe6874717b1657bf3edf026865826ff21
sahiljagad <sahiljagad@gmail.com>
2024-02-05 23:04:14 -0800

    feature: added data with id and rendered player cards

--- src/App.tsx
+++ src/App.tsx
@@ -5,43 +5,14 @@ import {
   Draggable,
   OnDragEndResponder,
 } from "react-beautiful-dnd";
+
+import { data } from "./data/Players";
 import "./App.css";
 
-const DATA = [
-  {
-    id: "0e2f0db1-5457-46b0-949e-8032d2f9997a",
-    name: "Walmart",
-    items: [
-      { id: "26fd50b3-3841-496e-8b32-73636f6f4197", name: "3% Milk" },
-      { id: "b0ee9d50-d0a6-46f8-96e3-7f3f0f9a2525", name: "Butter" },
-    ],
-    tint: 1,
-  },
-  {
-    id: "487f68b4-1746-438c-920e-d67b7df46247",
-    name: "Indigo",
-    items: [
-      {
-        id: "95ee6a5d-f927-4579-8c15-2b4eb86210ae",
-        name: "Designing Data Intensive Applications",
-      },
-      { id: "5bee94eb-6bde-4411-b438-1c37fa6af364", name: "Atomic Habits" },
-    ],
-    tint: 2,
-  },
-  {
-    id: "25daffdc-aae0-4d73-bd31-43f73101e7c0",
-    name: "Lowes",
-    items: [
-      { id: "960cbbcf-89a0-4d79-aa8e-56abbc15eacc", name: "Workbench" },
-      { id: "d3edf796-6449-4931-a777-ff66965a025b", name: "Hammer" },
-    ],
-    tint: 3,
-  },
-];
+let data2 = data.map((player, index) => ({ ...player, id: index }));
 
 function App() {
-  const [stores, setStores] = useState(DATA);
+  const [players, setPlayers] = useState(data2);
 
   const handleDragDrop: OnDragEndResponder = (results) => {
     const { source, destination, type } = results;
@@ -55,12 +26,12 @@ function App() {
       return;
 
     if (type === "group") {
-      const reorderedStores = [...stores];
+      const reorderedPlayers = [...players];
       const sourceIndex = source.index;
       const destinationIndex = destination.index;
-      const [removedStore] = reorderedStores.splice(sourceIndex, 1);
-      reorderedStores.splice(destinationIndex, 0, removedStore);
-      return setStores(reorderedStores);
+      const [removedPlayer] = reorderedPlayers.splice(sourceIndex, 1);
+      reorderedPlayers.splice(destinationIndex, 0, removedPlayer);
+      return setPlayers(reorderedPlayers);
     }
   };
 
@@ -74,10 +45,10 @@ function App() {
           <Droppable droppableId='ROOT' type='group'>
             {(provided) => (
               <div {...provided.droppableProps} ref={provided.innerRef}>
-                {stores.map((store, index) => (
+                {players.map((player, index) => (
                   <Draggable
-                    draggableId={store.id}
-                    key={store.id}
+                    draggableId={player.id.toString()}
+                    key={player.id}
                     index={index}
                   >
                     {(provided) => (
@@ -87,7 +58,7 @@ function App() {
                         {...provided.draggableProps}
                         ref={provided.innerRef}
                       >
-                        <h3>{store.name}</h3>
+                        <h3>{player.Name}</h3>
                       </div>
                     )}
                   </Draggable>
